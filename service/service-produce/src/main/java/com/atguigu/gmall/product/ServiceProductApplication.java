b22c4294922f70940adbeea917aa6006110ca2e2
wangjing <wangjing@qq.com>
2020-11-28 15:24:03 +0800

    44

--- service/service-produce/src/main/java/com/atguigu/gmall/product/ServiceProductApplication.java
+++ service/service-produce/src/main/java/com/atguigu/gmall/product/ServiceProductApplication.java
@@ -2,8 +2,11 @@ package com.atguigu.gmall.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan({"com.atguigu.gmall"})
 public class ServiceProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceProductApplication.class,args);
--- service/service-produce/src/main/java/com/atguigu/gmall/product/controller/CategoryApiController.java
+++ service/service-produce/src/main/java/com/atguigu/gmall/product/controller/CategoryApiController.java
@@ -1,14 +1,40 @@
 package com.atguigu.gmall.product.controller;
 
 import com.atguigu.gmall.common.result.Result;
+import com.atguigu.gmall.model.product.BaseCategory1;
+import com.atguigu.gmall.model.product.BaseCategory2;
+import com.atguigu.gmall.model.product.BaseCategory3;
+import com.atguigu.gmall.product.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("admin/product")
+@CrossOrigin
 public class CategoryApiController {
+
+    @Autowired
+    private CategoryService categoryService;
     @RequestMapping("getCategory1")
     public Result getCategory1(){
-        return Result.ok();
+        List<BaseCategory1> baseCategory1List= categoryService.getCategory1();
+        return Result.ok(baseCategory1List);
+    }
+
+    @RequestMapping("getCategory2/{category1Id}")
+    public Result getCategory2(@PathVariable("category1Id")Long category1Id){
+        List<BaseCategory2> baseCategory2List= categoryService.getCategory2(category1Id);
+        return Result.ok(baseCategory2List);
+    }
+
+    @RequestMapping("getCategory3/{category2Id}")
+    public Result getCategory3(@PathVariable("category2Id")Long category2Id){
+        List<BaseCategory3> baseCategory3List= categoryService.getCategory3(category2Id);
+        return Result.ok(baseCategory3List);
     }
 }
